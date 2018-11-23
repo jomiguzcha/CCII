a44a0d8cad47991ba88afe530d9079560c51582f
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-11-22 22:44:56 -0500

    Update GAUSS PUNTEROS.cpp

--- GAUSS PUNTEROS.cpp
+++ GAUSS PUNTEROS.cpp
@@ -9,87 +9,116 @@ void imprimir(double **arr) {
         for (int j = 0; j<m; j++) {
 		  cout<<*(*(arr + i)+j)<<" ";
 		  }
-		  cout<<endl;
+	   cout<<endl;
     }
+    cout << endl << endl << endl;
 }
 
-void operar_fila(double arr[][4], double a, int x, double b, int y) {
+void operar_fila(double **arr, double a, int x, double b, int y) {
     //matriz, coeficiente fila en la que se resta, fila en la que se resta, coeficiente fila que se resta, fila que se resta
-    int cont = 0;
-    for (double *i = *arr + 4; i<(*arr + x) + 7; i++, cont++) {
-	   *i = (a * (*i)) - (b * *((*arr + y) + cont));
+    for (int i = 0;i<m;i++) {
+	   *(*(arr + x) + i) = (*(*(arr + x) + i) * a) - (*(*(arr + y) + i) * b);
+	   //Coeficiente fila en la que se resta,fila en la que se resta,coeficiente fila que se resta, fila que se resta
     }
 }
 
-void to_one(double arr[][4], int i) {
-    double *a = *(arr + i) + i;
-    if (*a == 0) {
-	   *a = *a;
+void to_one(double **arr, int x) {
+    double a = 0;
+    if (*(*(arr + x) + x) == a) {
+	   a=0;
     }
     else {
 	   double aux;
-	   aux = 1 / *(*(arr + i) + i);
-	   for (int x = 0; x < 4; x++)
-		  *(*(arr + i) + x) *= aux;
+	   aux = 1 / *(*(arr + x) + x);
+	   for (int i = 0; i < m; i++)
+		  *(*(arr + x) + i) *= aux;
     }
 }
 
-/*void gauss(double arr[][4]) {
-    int aux = 0;
-    for (double(*i)[4] = arr; aux<3; aux++) {
-	   for (int cont = 0; i < arr + 3; i++, cont++) {
-		  if (*(*i + aux) == *(*(arr + cont) + cont) && *(*i + aux) != 1) {
-			 to_one(arr, cont);
-			 imprimir(arr, 12);
+void gauss(double **arr) {
+    for (int i = 0; i<(m-1); i++) {
+	   for (int j = 0; j<n; j++) {
+		  cout <<"J :"<< j <<"     I: "<< i << endl;
+		  if (i==j && *(*(arr + j) + j) != 1) {
+			 to_one(arr, j);
+			 imprimir(arr);
 			 continue;
 		  }
-
-		  else if ((*(*i + aux) == *(*(arr + cont) + cont) && *(*i + aux) == 1) || *(*i + aux) == 0) {
-			 imprimir(arr, 12);
+		  else if ((i == j && *(*(arr + j) + j) == 1) || (*(*(arr + j) + i) ==0)) {
+			 imprimir(arr);
 			 continue;
 		  }
-
-		  operar_fila(arr, 1, cont, *(*i + aux), 0);
-		  imprimir(arr, 12);
-
+		  operar_fila(arr, 1, j, *(*(arr + j) + i), i);
+		  imprimir(arr);
 	   }
-	   i = arr;
     }
-}*/
+}
 
 int main() {
     double x = 0, y = 0, z = 0;
-    //double matriz[3][4] = {{ 1,1,1,6 },
-    //							{ 2,-1,1,5 },
-     //                           {1,1,1,1}};
+    /*double matriz[3][4] = {{ 1,1,1,6 },
+    							{ 2,-1,1,5 },
+     							 {1,1,1,1}};*/
 
     double **matriz = new double*[n];
     for (int i = 0; i<n; i++) {
 		  *(matriz + i) = new double[m];
     }
-
-    for (int i = 0; i<n; i++) {
-        for (int j = 0; j<m; j++) {
-		  *(*(matriz + i)+j) = 1;}
+    
+    for (int i = 0; i < n; i++) {
+	   for (int j = 0; j < m; j++) {
+		  if (i == 0) {
+			 switch (j) {
+			 case 0:*(*(matriz + i) + j) = 1; break;
+			 case 1:*(*(matriz + i) + j) = 1; break;
+			 case 2:*(*(matriz + i) + j) = 1; break;
+			 case 3:*(*(matriz + i) + j) = 6; break;
+			 }
+		  }
+		  else if (i == 1) {
+			 switch (j) {
+			 case 0:*(*(matriz + i) + j) = 2; break;
+			 case 1:*(*(matriz + i) + j) = -1; break;
+			 case 2:*(*(matriz + i) + j) = 1; break;
+			 case 3:*(*(matriz + i) + j) = 5; break;
+			 }
+		  }
+		  else if (i == 2) {
+			 switch (j) {
+			 case 0:*(*(matriz + i) + j) = 3; break;
+			 case 1:*(*(matriz + i) + j) = 1; break;
+			 case 2:*(*(matriz + i) + j) = -2; break;
+			 case 3:*(*(matriz + i) + j) = 9; break;
+			 }
+		  }
+	   }
     }
+
+   /* for (int i = 0; i<n; i++) {
+		  for (int j = 0; j<m; j++) {
+			*(*(matriz + i)+j) = 2;
+		  }
+    }*/
 
     imprimir(matriz);
-    //gauss(matriz);
-
-    if (*(*matriz + 11) == 0) {
+    gauss(matriz);
+    
+    if (*(*(matriz + (n-1)) + (m-1)) == 0) {
 	   cout << "La matriz no tiene solucion" << endl;
+	   system("pause");
 	   return 0;
     }
-
-    z = *(*matriz + 11);
-    y = *(*matriz + 7) - *(*matriz + 6)* z;
-    x = *(*matriz + 3) - (*(*matriz + 1) * y + *(*matriz + 2) * z);
+    
+    z = *(*(matriz + (n - 1)) + (m - 1));
+    y = *(*(matriz + 1) + 3) - (*(*(matriz + 1) + 2) * z);
+    x = *(*(matriz) + 3) - (*(*(matriz) + 2) * z ) - (*(*(matriz)+1) * y);
 
     cout << "X = " << x << endl;
     cout << "Y = " << y << endl;
     cout << "Z = " << z << endl;
 
     delete matriz;
+    system("pause");
     return 0;
 }
 
