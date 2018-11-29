9d34bb13f9547e68d3c33049616c0f4c23a8fca3
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-11-29 18:46:57 -0500

    Update GAUSS PUNTEROS.cpp

--- GAUSS PUNTEROS.cpp
+++ GAUSS PUNTEROS.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int n=2;int m=4;
+int n = 4; int m = 5;
 
 void imprimir(double **arr) {
-  for (int i = 0; i<n; i++) {
-        for (int j = 0; j<m; j++) {
-		  cout<<*(*(arr + i)+j)<<" ";
-		  }
-	   cout<<endl;
+    for (int i = 0; i<n; i++) {
+	   for (int j = 0; j<m; j++) {
+		  cout << *(*(arr + i) + j) << " ";
+	   }
+	   cout << endl;
     }
     cout << endl << endl << endl;
 }
 
 void operar_fila(double **arr, double a, int x, double b, int y) {
     //matriz, coeficiente fila en la que se resta, fila en la que se resta, coeficiente fila que se resta, fila que se resta
-    for (int i = 0;i<m;i++) {
+    for (int i = 0; i<m; i++) {
 	   *(*(arr + x) + i) = (*(*(arr + x) + i) * a) - (*(*(arr + y) + i) * b);
 	   //Coeficiente fila en la que se resta,fila en la que se resta,coeficiente fila que se resta, fila que se resta
     }
@@ -25,7 +26,7 @@ void operar_fila(double **arr, double a, int x, double b, int y) {
 void to_one(double **arr, int x) {
     double a = 0;
     if (*(*(arr + x) + x) == a) {
-	   a=0;
+	   a = 0;
     }
     else {
 	   double aux;
@@ -36,14 +37,14 @@ void to_one(double **arr, int x) {
 }
 
 void gauss(double **arr) {
-    for (int i = 0; i<(m-1); i++) {
+    for (int i = 0; i<(m - 1); i++) {
 	   for (int j = 0; j<n; j++) {
-		  if (i==j && *(*(arr + j) + j) != 1) {
+		  if (i == j && *(*(arr + j) + j) != 1) {
 			 to_one(arr, j);
 			 imprimir(arr);
 			 continue;
 		  }
-		  else if ((i == j && *(*(arr + j) + j) == 1) || (*(*(arr + j) + i) ==0)) {
+		  else if ((i == j && *(*(arr + j) + j) == 1) || (*(*(arr + j) + i) == 0)) {
 			 imprimir(arr);
 			 continue;
 		  }
@@ -54,57 +55,65 @@ void gauss(double **arr) {
 }
 
 int main() {
-    double x = 0, y = 0, z = 0;
-    /*double matriz[3][4] = {{ 1,1,1,6 },
-    							{ 2,-1,1,5 },
-     							 {1,1,1,1}};*/
-
+    double x = 0, y = 0, z = 0, sum=0; int aux = 0;
+    string arr[] = {"Z:","X:","Y:","W:","V:","U:","T:","S:","R:","Q:" };
+   
     double **matriz = new double*[n];
     for (int i = 0; i<n; i++) {
-		  *(matriz + i) = new double[m];
+	   *(matriz + i) = new double[m];
     }
 
-    double *variables = new double[m-1];
+    double *variables = new double[m - 1];
+
 
     for (int i = 0; i < n; i++) {
 	   for (int j = 0; j < m; j++) {
-		  cout<<"Numero["<<i<<"]["<<j<<"]: ";
-		  cin>>*(*(matriz + i) + j);
+		  cout << "Numero[" << i << "][" << j << "]: ";
+		  cin >> *(*(matriz + i) + j);
 	   }
     }
 
-   /* for (int i = 0; i<n; i++) {
-		  for (int j = 0; j<m; j++) {
-			*(*(matriz + i)+j) = 2;
-		  }
+    /* for (int i = 0; i<n; i++) {
+    for (int j = 0; j<m; j++) {
+    *(*(matriz + i)+j) = 2;
+    }
     }*/
 
     imprimir(matriz);
     gauss(matriz);
 
-    if (*(*(matriz + (n-1)) + (m-2)) == 0) {
+    if (*(*(matriz + (n - 1)) + (m - 2)) == 0) {
 	   cout << "La matriz no tiene solucion" << endl;
+	   system("pause");
 	   return 0;
     }
+    
+    for (int i = 0; i < (m - 1); i++) {
+	   double *var = variables;
+	   if (i == 0) {
+		  *(var + i) = *(*(matriz + (n - 1)) + (m - 1));
+		  continue;
+	   }
 
-    for(int i=0;i<(m-1);i++){
-        if(i==0){
-            variables[i]=*(*(matriz + (n - 1)) + (m - 1));
-            continue;
-        }
-        
-        variables[i]=*(*(matriz + (n-(i+1)))+ n);
+	   sum = *(*(matriz + (n - (i + 1))) + (m - 1)) - (*(*(matriz + (n - (i + 1))) + (m - 2)) * *(var + aux));
+
+	   if (i > 1) {
+		  for (int a = 0; a < (i - 1); a++) {
+			 sum -= (*(*(matriz + (n - (i + 1))) + (m - (a+3))) *  *(var +(a+1)));}
+	   }
+	   *(var + i) = sum;
+	   sum = 0;
     }
 
-    z = *(*(matriz + (n - 1)) + (m - 1));
-    y = *(*(matriz + 1) + 3) - (*(*(matriz + 1) + 2) * z);
-    x = *(*(matriz) + 3) - (*(*(matriz) + 2) * z ) - (*(*(matriz)+1) * y);
-
-    cout << "X = " << x << endl;
-    cout << "Y = " << y << endl;
-    cout << "Z = " << z << endl;
+    for (int i = 0; i < (m - 1); i++) {
+	   double *var = variables; string *ar = arr;
+	   cout << *(ar+i);
+	   cout << *(var + i) <<"\t";
+    }
+    cout << endl;
 
     delete matriz;
+    system("pause");
     return 0;
 }
 
